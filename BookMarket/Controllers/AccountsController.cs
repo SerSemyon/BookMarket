867e60edf5daec2e345bcb083f66cbbaa89b091a
NTG <nikolay.tep@yandex.ru>
2023-04-26 15:47:27 +0300

    Add Accounts controller

---
+++ BookMarket/Controllers/AccountsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookMarket;
+
+namespace BookMarket.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly DbbookMarketContext _context;
+
+        public AccountsController() // DbbookMarketContext context
+        {
+            _context = new DbbookMarketContext();
+        }
+
+        // GET: Accounts
+        public async Task<IActionResult> Index()
+        {
+            var dbbookMarketContext = _context.Accounts.Include(a => a.Type);
+            return View(await dbbookMarketContext.ToListAsync());
+        }
+
+        // GET: Accounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .Include(a => a.Type)
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: Accounts/Create
+        public IActionResult Create()
+        {
+            ViewData["TypeId"] = new SelectList(_context.AccountTypes, "TypeId", "TypeId");
+            return View();
+        }
+
+        // POST: Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AccountId,TypeId,AccName,AccLastName,AccMiddleName,AccGender,AccBirthday,AccEmail,AccPhoneRegistration,AccHashPassword")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(account);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.AccountTypes, "TypeId", "TypeId", account.TypeId);
+            return View(account);
+        }
+
+        // GET: Accounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            ViewData["TypeId"] = new SelectList(_context.AccountTypes, "TypeId", "TypeId", account.TypeId);
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AccountId,TypeId,AccName,AccLastName,AccMiddleName,AccGender,AccBirthday,AccEmail,AccPhoneRegistration,AccHashPassword")] Account account)
+        {
+            if (id != account.AccountId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.AccountId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.AccountTypes, "TypeId", "TypeId", account.TypeId);
+            return View(account);
+        }
+
+        // GET: Accounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .Include(a => a.Type)
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Accounts == null)
+            {
+                return Problem("Entity set 'DbbookMarketContext.Accounts'  is null.");
+            }
+            var account = await _context.Accounts.FindAsync(id);
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountExists(int id)
+        {
+          return (_context.Accounts?.Any(e => e.AccountId == id)).GetValueOrDefault();
+        }
+    }
+}
