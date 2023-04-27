04bf0d299d7687216b4ffedc863d1cf4c21fa225
NTG <nikolay.tep@yandex.ru>
2023-04-27 17:58:03 +0300

    Издательства отображается по имени в Books

--- BookMarket/Controllers/BooksController.cs
+++ BookMarket/Controllers/BooksController.cs
@@ -25,8 +25,10 @@ namespace BookMarket.Controllers
                 .Include(b => b.LegalEntity)
                 .Include(b => b.Phouse)
                 .FirstOrDefaultAsync(m => m.BookId == id);
-            ViewBag.feedbacks = _context.Feedbacks.Where(f => f.BookId == id).Include(f => f.Account).ToList();
-            //_context.Feedbacks.ToList()[0]
+
+            ViewBag.feedbacks = _context.Feedbacks.Where(f => f.BookId == id)
+                .Include(f => f.Account)
+                .ToList();
             return View(book);
         }
 
